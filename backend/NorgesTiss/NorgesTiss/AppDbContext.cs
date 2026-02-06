bca9f8ddf7d2d433639496e0ff22afc53422c4cc
IkayImonoko <ntolok1990@gmail.com>
2026-02-06 17:38:52 +0100

    add ef

---
+++ backend/NorgesTiss/NorgesTiss/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NorgesTiss;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    // Таблицы проекта
+    public DbSet<PublicToilet> PublicToilets { get; set; }
+    
+}
--- backend/NorgesTiss/NorgesTiss/Program.cs
+++ backend/NorgesTiss/NorgesTiss/Program.cs
@@ -1,6 +1,21 @@
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
+using NorgesTiss;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (builder.Environment.IsDevelopment())
+{
+    // Локально — SQLite
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlite(connectionString));
+}
+else
+{
+    // Сервер — PostgreSQL
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+}
 //1 Controllers
 builder.Services
     .AddControllers()
