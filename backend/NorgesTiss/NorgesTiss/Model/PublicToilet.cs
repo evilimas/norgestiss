515d9cde315af62a01133133087f488db9cdb02b
IkayImonoko <ntolok1990@gmail.com>
2026-02-12 10:33:58 +0100

    rollback publicktoilet model class

---
+++ backend/NorgesTiss/NorgesTiss/Model/PublicToilet.cs
@@ -0,0 +1,13 @@
+﻿public class PublicToilet
+{
+    public int Id { get; set; } 
+    
+    public string Name { get; set; }
+    public double Latitude { get; set; }  
+    public double Longitude { get; set; }
+    public string Adress { get; set; }
+    
+    public bool IsFree { get; set; } 
+    public bool HasHandicapAccess { get; set; }
+    public string Description { get; set; }
+}
