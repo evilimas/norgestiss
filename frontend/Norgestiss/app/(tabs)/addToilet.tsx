c55c8a918ac35317857bec80bdc0066b9bb2ce9a
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-05 10:32:32 +0100

    new tab add toilets

---
+++ frontend/Norgestiss/app/(tabs)/addToilet.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const addToilet = () => {
+  return (
+    <View>
+      <Text>addToilet</Text>
+    </View>
+  );
+};
+
+export default addToilet;
